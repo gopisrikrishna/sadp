c510aa71288ffb00e861203f8474a9de70e46301
gopisrikrishna <51519189+gopisrikrishna@users.noreply.github.com>
2022-01-27 23:55:00 +0530

    Update MediatorPatternDemo2.java

--- MediatorPatternDemo2.java
+++ MediatorPatternDemo2.java
@@ -27,3 +27,13 @@ public class MediatorPatternDemo2 {
  john.sendMessage("Hello! Robert!");
  }
 }
+
+/*
+D:\41lab\sadp\11_prog_MediatorDesignPattern>javac MediatorPatternDemo2.java
+
+D:\41lab\sadp\11_prog_MediatorDesignPattern>java MediatorPatternDemo2
+Thu Jan 27 23:53:35 IST 2022 [Robert] : Hi! John!
+Thu Jan 27 23:53:35 IST 2022 [John] : Hello! Robert!
+
+D:\41lab\sadp\11_prog_MediatorDesignPattern>
+*/
